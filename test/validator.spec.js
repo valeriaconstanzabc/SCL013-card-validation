8e3ab73bad8965763265e7b82be8d5d3e11a8751
Niennis <estefania.osses.v@gmail.com>
2020-03-27 12:25:28 -0300

    first commit

---
+++ test/validator.spec.js
@@ -0,0 +1,28 @@
+// importamos el objeto `validator`, que contiene las funciones `isValid` y `maskify`
+import validator from '../src/validator';
+
+describe('validator', () => {
+  test('debería ser un objeto', () => {
+    expect(typeof validator).toBe('object');
+  });
+
+  describe('validator.isValid', () => {
+    test('debería ser una función', () => {
+      expect(typeof validator.isValid).toBe('function');
+    });
+
+    test.skip('debería retornar true para "4083952015263"', () => {
+      // escribe aquí tu test
+    });
+  });
+
+  describe('validator.maskify', () => {
+    test('debería ser una función', () => {
+      expect(typeof validator.maskify).toBe('function');
+    });
+
+    test.skip('Debería retornar "############5616" para "4556364607935616"', () => {
+      // escribe aquí tu test
+    });
+  });
+});
