f6b429fcffc02b26e5951329161c91f8d79f25c7
Niamoyano91 <62710894+Niamoyano91@users.noreply.github.com>
2020-04-07 09:07:32 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import validator from './validator.js';
 
+//Variable que trae el botón validar desde HTML
+let botonValidar = document.getElementById("validar");
+//Botón validar (click en el objeto botón)
+botonValidar.addEventListener('click', (validar)=>{
+  //devuelve una cadena que representa el objeto
+  (validar).preventDefault();
+  //Variable que extrae el número de la tarjeta ingresada
+  let numberCard = document.getElementById('tarjeta').value;
+  validator.isValid(numberCard);
+});
+
+let botonConfirmaReserva = document.getElementById("botonConfirmarReserva");
+botonConfirmarReserva.addEventListener('click', ()=>{
+  window.location.href = "confirmation.html";
+  let numberCard = document.getElementById('tarjeta').value;
+  validator.maskify(numberCard);
+  document.getElementById("tarjetaOculta").innerHTML =validator.maskify();
+});
+
+  
+
+
 console.log(validator);
