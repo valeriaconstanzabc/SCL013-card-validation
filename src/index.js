7c1a2a4a5aea71c0fbe57a7141a7641b3d20ce37
valeriaconstanzabc <62715732+valeriaconstanzabc@users.noreply.github.com>
2020-04-07 09:47:32 -0400

    Index js.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+//Llamamos aL objeto para que arroje el resultado
 import validator from './validator.js';
+//Variable que trae el botón validar desde HTML
+let botonValidar = document.getElementById("validar");
+//Botón validar (click en el objeto botón)
+botonValidar.addEventListener('click', (validar)=>{
+  //devuelve una cadena que representa el objeto
+  (validar).preventDefault();
+  //Variable que extrae el número de la tarjeta ingresada
+  let numberCard = document.getElementById('tarjeta').value;
+  validator.isValid(numberCard);
+});
 
-console.log(validator);
+let botonConfirmaReserva = document.getElementById("botonConfirmarReserva");
+botonConfirmarReserva.addEventListener('click', ()=>{
+  window.location.href = "confirmation.html";
+  let numberCard = document.getElementById('tarjeta').value;
+  validator.maskify(numberCard);
+  document.getElementById("tarjetaOculta").innerHTML = "holi" + validator.maskify();
+});
+
+  
